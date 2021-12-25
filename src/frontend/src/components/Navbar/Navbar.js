9a606374c47aeff6f137e5afdc6950b469e486a3
gusscamargo <guga.power@hotmail.com>
2021-12-25 06:13:46 -0300

    Ajustando navbar

--- src/frontend/src/components/Navbar/Navbar.js
+++ src/frontend/src/components/Navbar/Navbar.js
@@ -1,38 +1,34 @@
-import { Navbar as NavbarMaterialize, NavItem, Icon } from "react-materialize"
+import { Navbar as NavbarMaterialize, NavItem, Icon} from "react-materialize"
 
 const Navbar = ({desafioUrl, linkedIn, github}) => {
 
+    const classNavbar = "indigo darken-4 z-depth-0"
+    const classButtonsMenus = "btn-large waves-effect waves-light blue lighten-2"
+    const classButtonsLinks = "btn waves-effect waves-light red darken-4"
+
     return (
         <NavbarMaterialize
+            className={classNavbar}
             alignLinks="right"
             brand={
                 <a className="brand-logo" target="_blank" href={desafioUrl}>Desafio-FullStack / Gazin Tech</a>
             }
             extendWith={
-            <NavbarMaterialize>
-                <NavItem href="/">Home</NavItem>
-                <NavItem href="/nivel">Nivel</NavItem>
-                <NavItem href="/desenvolvedor">Desenvolvedor</NavItem>
+            <NavbarMaterialize className={classNavbar}>
+                <NavItem className={classButtonsMenus} href="/">Home</NavItem>
+                <NavItem className={classButtonsMenus} href="/nivel">Nivel</NavItem>
+                <NavItem className={classButtonsMenus} href="/desenvolvedor">Desenvolvedor</NavItem>
             </NavbarMaterialize>
             }
             id="mobile-nav"
             menuIcon={<Icon>menu</Icon>}
-            options={{
-                draggable: true,
-                edge: 'left',
-                inDuration: 250,
-                onCloseEnd: null,
-                onCloseStart: null,
-                onOpenEnd: null,
-                onOpenStart: null,
-                outDuration: 200,
-                preventScrolling: true
-            }}
             >
-            <NavItem href={github} target="_blank">
+            <NavItem className={classButtonsLinks} href={github} target="_blank">
+                <i className="material-icons left">code</i>
                 Meu Github
             </NavItem>
-            <NavItem href={linkedIn} target="_blank">
+            <NavItem className={classButtonsLinks} href={linkedIn} target="_blank">
+                <i className="material-icons left">account_circle</i>
                 Meu LinkedIn
             </NavItem>
         </NavbarMaterialize>

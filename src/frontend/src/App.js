73c0b6d69a5b6c941654eb8d363ec3d65bb08e7d
gusscamargo <guga.power@hotmail.com>
2021-12-25 16:49:14 -0300

    Agora Content fica como modulo

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,22 +1,14 @@
-import Header from "./components/Header/Header";
-import {Route, BrowserRouter as Router, Routes} from "react-router-dom"
+import {BrowserRouter as Router} from "react-router-dom"
 
-import Home from "./pages/Home/HomePage"
-import Nivel from "./pages/Nivel/NivelPage"
-import Desenvolvedor from "./pages/Desenvolvedor/DesenvolvedorPage"
+import Header from "./components/Header/Header";
+import Content from "./components/Content/Content";
 
 function App(props) {
   return (
     <Router> 
       <Header />
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/nivel" element={<Nivel />} />
-          <Route path="/desenvolvedor" element={<Desenvolvedor />} />
-        </Routes>
-      </div>
-      </Router>
+      <Content />
+    </Router>
   );
 }
 
---
+++ src/frontend/src/components/Content/Content.js
@@ -0,0 +1,17 @@
+import {Route, Routes} from "react-router-dom"
+
+import Home from "../../pages/Home/HomePage"
+import Nivel from "../../pages/Nivel/NivelPage"
+import Desenvolvedor from "../../pages/Desenvolvedor/DesenvolvedorPage"
+
+export default function Content() {
+    return (
+        <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/nivel" element={<Nivel />} />
+          <Route path="/desenvolvedor" element={<Desenvolvedor />} />
+        </Routes>
+      </div>
+    )
+}

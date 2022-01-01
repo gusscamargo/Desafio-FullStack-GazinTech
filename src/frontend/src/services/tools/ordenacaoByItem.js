5804c5992ba0909d20ec02b436a71067d377dff0
gusscamargo <guga.power@hotmail.com>
2022-01-01 20:28:41 -0300

    Ajustes

--- src/frontend/src/services/tools/ordenacaoByItem.js
+++ src/frontend/src/services/tools/ordenacaoByItem.js
@@ -1,10 +1,12 @@
 export const ordenacaoByItem = (data, selectOrdenacao, selectFormaOrndenacao) => {
+
+    
     if (selectOrdenacao === "nenhuma"){
         return [...data]
     }else{
+        console.log(selectFormaOrndenacao)
         if (selectFormaOrndenacao === "crescente") {
             return [...data].sort((a, b) => (eval(`a.${selectOrdenacao} > b.${selectOrdenacao}`)) ? 1 : (eval(`b.${selectOrdenacao} > a.${selectOrdenacao}`) ? -1 : 0))
-
         }
 
         return [...data].sort((a, b) => (eval(`a.${selectOrdenacao} < b.${selectOrdenacao}`)) ? 1 : (eval(`b.${selectOrdenacao} < a.${selectOrdenacao}`) ? -1 : 0))

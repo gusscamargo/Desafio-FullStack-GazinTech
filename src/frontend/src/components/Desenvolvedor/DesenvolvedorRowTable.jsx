59bc924a9771dd4947d49059b81a8e4c1dde92f7
gusscamargo <guga.power@hotmail.com>
2022-01-17 09:54:15 -0300

    Excluindo build e tornando mais palpavel ver data de nascimento no modal do desenvolvedor

--- src/frontend/src/components/Desenvolvedor/DesenvolvedorRowTable.jsx
+++ src/frontend/src/components/Desenvolvedor/DesenvolvedorRowTable.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Textarea } from 'react-materialize'
 import Acoes from '../Acoes'
+import { convertDbFormatToHuman } from '../../services/tools/convertFormatTime'
 
 export default function DesenvolvedorRowTable({data = []}) {
     if (data.length > 0) {
@@ -16,7 +17,7 @@ export default function DesenvolvedorRowTable({data = []}) {
                                 <div className='container'>
                                     <h5><strong>Nome:</strong> {item.nome}</h5>
                                     <h5><strong>Sexo:</strong> {item.sexo}</h5>
-                                    <h5><strong>Data de nascimento:</strong> {item.datanascimento}</h5>
+                                    <h5><strong>Data de nascimento:</strong> {convertDbFormatToHuman(item.datanascimento)}</h5>
                                     <h5><strong>Idade:</strong> {item.idade}</h5>
                                     <h5><strong>Nivel:</strong> {item.Nivel.nivel}</h5>
                                     <h5><strong>Hobby:</strong> {item.hobby === "" ? "Hobbies não informados" : ""}</h5>
